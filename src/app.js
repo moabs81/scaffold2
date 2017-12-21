c2764001320536a9b154e8cef5952c743a7234bf
moabs81 <moabs81@hotmail.com>
2017-12-21 09:10:28 -0500

    fixed a small problem in app.js

--- src/app.js
+++ src/app.js
@@ -6,6 +6,5 @@ import { demoApp } from './components/application/app'; //import app entry point
 buildTableComponent(function(result) { //callback function returns the DOM target for your app   
     const targetDiv = '#' + result,
         demoAppText = demoApp();
-    console.log(targetDiv);
-    $('#bddAppContainer').append('<p style="color:#838b83; font-family: sans-serif; font-weight: bold;">' + demoAppText + '</p>');
+    $(targetDiv).append('<p style="color:#838b83; font-family: sans-serif; font-weight: bold;">' + demoAppText + '</p>');
 });
